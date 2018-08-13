56e393d5bd2cdb1296a1de7204a04ba9397e2c53
LabElec42 <fherbine>
2018-08-13 19:18:38 +0200

    button in progress interrupt segfault when button is overused

--- Software.X/interrupt.c
+++ Software.X/interrupt.c
@@ -8,37 +8,44 @@ void __ISR (_TIMER_2_VECTOR, IPL7SRS) led_blink (void){
 	LATFbits.LATF1 = 0;
 	IFS0bits.T2IF = 0;
 }
-
 void __ISR (_EXTERNAL_2_VECTOR, IPL6SRS) left_click (void){
+        //IEC0bits.INT2IE = 0;
  	T2CONbits.ON = 0;
-	IFS0bits.INT1IF = 0;
 	LATFbits.LATF1 = 1;
-	//send_report(create_report(1, 0, 0));	//
+	send_report(create_report(1, 0, 0));
         TMR2 = 0;
         T2CONbits.ON = 1;
+        IFS0bits.INT2IF = 0;
 }
 
 void __ISR (_EXTERNAL_3_VECTOR, IPL5SRS) right_click (void){
+        //IEC0bits.INT3IE = 0;
 	T2CONbits.ON = 0;
-	IFS0bits.INT3IF = 0;
 	LATFbits.LATF1 = 1;
-	//send_report(create_report(2, 0, 0));
+	send_report(create_report(2, 0, 0));
         TMR2 = 0;
 	T2CONbits.ON = 1;
+        IFS0bits.INT3IF = 0;
 }
 
 void set_external_interrupt() {
- 
+
 	// Interrupt Button left click
-	INTCONbits.INT2EP = 0;
+        //IEC0CLR = 0x00008000; // disable INT3
+        //INTCONCLR = 0x00000008; // clear the bit for falling edge trigger
+        //IFS0CLR = 0x00008000; // clear the interrupt flag
+        //IEC0SET = 0x00008000; // enab;e INT3
+
+	INTCONbits.INT2EP = 0;// Falling edge 1-> Rising egde
 	IPC2bits.INT2IP = 6;
-        IPC1bits.INT1IS = 2;
+        //IPC2bits.INT2IS = 2;
 	IFS0bits.INT2IF = 0;
 	IEC0bits.INT2IE = 1;
 
 	// Interrupt Button right click
 	INTCONbits.INT3EP = 0;
 	IPC3bits.INT3IP = 5;
+        //IPC3bits.INT3IS = 1;
 	IFS0bits.INT3IF = 0;
 	IEC0bits.INT3IE = 1;
 
@@ -52,35 +59,26 @@ void set_timer() {
         IFS0bits.T2IF = 0;
         IPC2bits.T2IP = 7;
         IEC0bits.T2IE = 1;
-        //T2CONbits.ON = 1;
 }
-
-//void set_interrupt() { //final function
-//	set_external_interrupt();
-//	set_timer();
-//
-//	INTCONbits.MVEC = 1;
-//	__builtin_enable_interrupts();
-//
-//	//T2CONbits.ON = 1;
-//}
 
 void __ISR (_EXTERNAL_1_VECTOR, IPL6SRS) test_click (void){
-	T2CONbits.ON = 0;
-	IFS0bits.INT1IF = 0;
-	LATFbits.LATF1 = 1;
-	//send_report(create_report(2, 0, 0));	//
-        TMR2 = 0;
-        T2CONbits.ON = 1;
+	//T2CONbits.ON = 0;
+	//IFS0bits.INT1IF = 0;
+	LATFbits.LATF1 ^= 1;
+	send_report(create_report(2, 0, 0));
+        send_report(create_report(0, 0, 0));
+        IFS0bits.INT1IF = 0;
+        //TMR2 = 0;
+	//T2CONbits.ON = 1;
 }
 
-void set_interrupt() { //test funtction with button on the board
-	INTCONbits.INT1EP = 0;
-	IPC1bits.INT1IP = 6;
-	IPC1bits.INT1IS = 0;
-	IFS0bits.INT1IF = 0;
-	IEC0bits.INT1IE = 1;
-
+void set_interrupt() { //final function
+//  Test interrupt with board button uncomment this line and comment set_external & set_timer
+//    	INTCONbits.INT1EP = 0;
+//	IPC1bits.INT1IP = 6;
+//	IFS0bits.INT1IF = 0;
+//	IEC0bits.INT1IE = 1;
+        set_external_interrupt();
 	set_timer();
 
 	INTCONbits.MVEC = 1;
